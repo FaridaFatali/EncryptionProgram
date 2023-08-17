91e06eb59948d19abce81bde53d61199deae1b89
FaridaFatali <faridafatali.91@gmail.com>
2023-08-17 09:53:51 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Practice: <a href="https://www.youtube.com/watch?v=jjcJCvIoj2M&list=PLZPZq0r_RZOMhCAyywfnYLlrjiVOkdAI1&index=90">...</a>
+ * @author Farida Fatali
+ * Creating Encryption/Decryption Program using ASCII Table (in Java)
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        EncryptionProgram ep = new EncryptionProgram();
+    }
+}
